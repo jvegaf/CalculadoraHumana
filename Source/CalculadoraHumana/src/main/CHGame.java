9953798c9ddd4e35d82649fe55d323084d78a185
Jose Vega <josevega234@gmail.com>
2018-11-27 09:33:16 +0100

    Created source code project, classes and test files

---
+++ Source/CalculadoraHumana/src/main/CHGame.java
@@ -0,0 +1,9 @@
+package main;
+
+public class CHGame {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ Source/CalculadoraHumana/src/test/CHTimeTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CHTimeTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
